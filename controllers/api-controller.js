f690e050a04a4e7c45189da91d5da0fbfafd0fcc
onetwomark <mark_i93@hotmail.co.uk>
2025-01-27 14:41:27 +0000

    remove redundant console.logs

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -2,7 +2,6 @@ const {selectEndpoints} = require('../models/api.model')
 const endpointsJson = require("../endpoints.json");
 
 getApi = (req, res, next) => {
-    console.log(endpointsJson)
    return res.status(200).send({ endpoints: endpointsJson })
 }
 
