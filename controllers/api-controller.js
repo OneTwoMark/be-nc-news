c46ff427ff22bdedb9f563492565e0df552246f7
onetwomark <mark_i93@hotmail.co.uk>
2025-01-27 23:33:42 +0000

    finds and returns article with ID, checks for correct type and properties, passes error handling

--- controllers/api-controller.js
+++ controllers/api-controller.js
@@ -1,4 +1,4 @@
-const {selectEndpoints, formatTopics} = require('../models/api.model')
+const {selectArticleById} = require('../models/api.model')
 const endpointsJson = require("../endpoints.json");
 const testData = require('../db/data/test-data')
 
@@ -10,4 +10,14 @@ getTopics = (req, res, next) => {
     return res.status(200).send(testData.topicData)
 }
 
-module.exports = {getApi, getTopics};
+getArticleById = (req, res, next) => {
+    const {article_id} = req.params;
+    selectArticleById(article_id).then((article) => {
+        res.status(200).send(article)
+    })
+    .catch((err) => {
+        console.log("Error for getArticleById")
+        next(err)
+    })
+}
+module.exports = {getApi, getTopics, getArticleById};
--- models/api.model.js
+++ models/api.model.js
@@ -1,6 +1,28 @@
 const db = require('../db/connection');
 const endpointsJSON = require("../endpoints.json");
 
+selectArticleById = (article_id) => {
+    return db
+    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .then((result) => {
+        if (typeof Number(article_id) !== "number"){
+            console.log("not a number")
+            return Promise.reject({
+                status: 400,
+                msg: "Bad Request"
+            })
+        }
+        else if (result.rows.length === 0) {
+            console.log("Article_ID not found")
+            return Promise.reject({
+                status: 404,
+                msg: "ID Not found"
+            })
+        } else {
+            console.log("ID found")
+            return result.rows;
+        }
+    })
+}
 
-
-module.exports = {};
+module.exports = {selectArticleById};
