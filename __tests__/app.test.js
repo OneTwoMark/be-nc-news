2f88e61f85a4db3212c1a0eb17449e2566c69962
onetwomark <mark_i93@hotmail.co.uk>
2025-01-27 17:18:07 +0000

    will output topics from given endpoint, 3 tests to check for correct type, properties and error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,10 +3,11 @@ const app = require ('../app.js')
 const request = require('supertest');
 const db = require('../db/connection.js');
 const seed = require('../db//seeds/seed.js');
+const testData = require('../db/data/test-data')
 /* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
-// beforeEach(() => seed({endpointsJson}));
+// beforeEach(() => seed());
 // afterAll(() => db.end())
 
 describe("GET /api", () => {
@@ -19,3 +20,32 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe('GET /api/topics', () => {
+  test('200: Responds with array of topic objects', () => {
+    return request(app)
+    .get('/api/topics')
+    .expect(200)
+    .then((response) => {
+      expect(response.body).toEqual(testData.topicData)
+    })
+  });
+  test('should have slug and description properties', () => {
+    return request(app)
+    .get('/api/topics')
+    .expect(200)
+    .then((response) => {
+      response.body.forEach((topic) => {
+        expect(topic).toHaveProperty('slug')
+        expect(topic).toHaveProperty('description')
+      })
+    })
+  });
+  test('404: responds with error when endpoint not found', () => {
+    return request(app)
+    .get('/api/hello')
+    .expect(404)
+    .then((response) => {
+      expect(response.body).toEqual({"error": "Endpoint not found"})
+  })
+})})
--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require("express")
 const app = express()
-const getApi = require('./controllers/api-controller.js')
+const {getApi, getTopics} = require('./controllers/api-controller.js')
+const testData = require('./db/data/test-data')
 
 app.use(express.json());
 
 app.get('/api', getApi)
 
+app.get('/api/topics', getTopics)
+
+
+// look into app.all(/*)
+
+
+app.all('*', (req, res) => {
+    console.log("Error: 404")
+    res.status(404).send({error: "Endpoint not found"})
+})
+
 app.use((err, req, res, next) => {
     console.log("This is error handling block 1")
+    console.log("Error code: ", err.code)
 })
+
+app.use((err, req, res, next) => {
+    console.log("Error: 500")
+    console.log("Error code: ", err.code)
+    res.status(500).send({error: "Internal Server Error"})
+})
+
 
 module.exports = app;
